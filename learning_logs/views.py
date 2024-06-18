69cccaeaf01b58268be93c744ab678fd078bff45
yeezy <temirlan.10.2004@gmail.com>
2024-06-18 21:13:02 +0600

    Fixing the topic view.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -21,7 +21,7 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
 	"""Show a singel topic and all its entries."""
-	topic = get_object_or_404(Topic, id=topic_id)
+	topic = get_object_or_404(Topic, id=topic_id) 
 
 	# Make sure the topic belongs to the current user.
 	if topic.owner != request.user:
